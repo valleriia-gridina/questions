b2ecbfd024afb0c0d3fff47fb663f48a26c241eb
Roman Reznikov <roman.reznikov@dataart.com>
2024-10-30 13:37:23 +0100

    v.2.0.1

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div>
-      404 Not Found
-      <Link to="/">Home</Link>
+    <div className="page404">
+      <span className="notFound">404 Page not found</span>
+      <Link className="link" to="/">
+        Back to home
+      </Link>
     </div>
   );
 }
