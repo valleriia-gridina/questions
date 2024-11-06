72fd0c446a7d250064800735ae0a8fbb962a2e0d
Roman Reznikov <roman.reznikov@dataart.com>
2024-11-06 17:39:57 +0100

    code fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {}
+import HomePage from "./pages/HomePage.tsx";
+import QuestionsPage from "./pages/Questions.tsx";
+import ResultsPage from "./pages/Results.tsx";
+import NotFound from "./pages/NotFound.tsx";
+import "./App.css";
+import "./index.css";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+      errorElement: <NotFound />,
+    },
+    {
+      path: "/questions",
+      element: <QuestionsPage />,
+      errorElement: <NotFound />,
+    },
+    {
+      path: "/results",
+      element: <ResultsPage />,
+      errorElement: <NotFound />,
+    },
+  ]);
+  <RouterProvider router={router} />;
+}
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import HomePage from "./pages/HomePage.tsx";
-import QuestionsPage from "./pages/Questions.tsx";
-import ResultsPage from "./pages/Results.tsx";
-import "./App.css";
-import "./index.css";
-import NotFound from "./pages/NotFound.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/questions",
-    element: <QuestionsPage />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/results",
-    element: <ResultsPage />,
-    errorElement: <NotFound />,
-  },
-]);
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
